4583006620d38fd88ff55b27b2696adff2c8ccb3
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-11-08 12:25:24 +0100

    gitignore updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,40 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // 1. Modo
-  mode: "development",
+  mode: "production",
 
-  // 2. Punto de entrada
   entry: "./src/index.js",
 
-  // 3. Configuración del servidor de desarrollo
   devServer: {
-    static: "./dist", // Carpeta que el servidor observará
-    open: true, // Abre el navegador automáticamente
+    static: "./dist",
+    open: true,
   },
 
-  // 4. Source Maps (para depurar)
   devtool: "eval-source-map",
 
-  // 5. Salida (Output)
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // Limpia la carpeta 'dist' antes de cada build
+    clean: true,
+    publicPath: "/todo-list/",
   },
 
-  // 6. Plugins
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html", // Tu plantilla HTML
+      template: "./src/template.html",
+      filename: "index.html",
     }),
   ],
 
-  // 7. Módulos (para los loaders)
   module: {
     rules: [
       {
-        test: /\.css$/i, // Regex para archivos .css
-        use: ["style-loader", "css-loader"], // Loaders que se usarán
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Para imágenes
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
     ],
